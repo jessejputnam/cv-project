62e4fd822158846e061e99ec43d6c3af30e8737e
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-06-03 13:59:36 -0400

    Convert General and Experience forms to functional React

--- src/components/ExperienceForm.js
+++ src/components/ExperienceForm.js
@@ -1,24 +1,11 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class ExperienceForm extends Component {
-  constructor() {
-    super();
+const ExperienceForm = function (props) {
+  const onCancel = (e) => props.parentCallbackExpCancel(false);
 
-    this.state = {
-      company: "",
-      position: "",
-      tasks: [""],
-      dateFrom: "",
-      dateTo: ""
-    };
-  }
-
-  onCancel = (e) => {
-    this.props.parentCallbackExpCancel(false);
-  };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
+
     const data = {
       company: e.target.companyInput.value,
       position: e.target.positionInput.value,
@@ -30,99 +17,87 @@ class ExperienceForm extends Component {
       dateTo: e.target.expDateTo.value
     };
 
-    this.props.parentCallbackExp(data);
+    props.parentCallbackExp(data);
   };
 
-  render() {
-    return (
-      <div className='form'>
-        <form onSubmit={this.onSubmit}>
-          <div className='form__row'>
-            <label>Company: </label>
-            <input
-              className='text-input'
-              type='text'
-              id='companyInput'
-              name='companyInput'
-              placeholder='Company Name'
-              defaultValue={
-                this.props.data === undefined ? "" : this.props.data.company
-              }
-            ></input>
-          </div>
+  return (
+    <div className='form'>
+      <form onSubmit={onSubmit}>
+        <div className='form__row'>
+          <label>Company: </label>
+          <input
+            className='text-input'
+            type='text'
+            id='companyInput'
+            name='companyInput'
+            placeholder='Company Name'
+            defaultValue={props.data === undefined ? "" : props.data.company}
+          ></input>
+        </div>
 
-          <div className='form__row'>
-            <label>Position: </label>
-            <input
-              className='text-input'
-              type='text'
-              id='positionInput'
-              name='positionInput'
-              placeholder='Senior position'
-              defaultValue={
-                this.props.data === undefined ? "" : this.props.data.position
-              }
-            ></input>
-          </div>
+        <div className='form__row'>
+          <label>Position: </label>
+          <input
+            className='text-input'
+            type='text'
+            id='positionInput'
+            name='positionInput'
+            placeholder='Senior position'
+            defaultValue={props.data === undefined ? "" : props.data.position}
+          ></input>
+        </div>
 
-          <div className='form__row form__row--tasks'>
-            <label>
-              Main Tasks:
-              <br />
-              <span className='small-text'>Separate with periods</span>
-            </label>
-            <input
-              className='text-input'
-              type='text'
-              name='tasksInput'
-              id='tasksInput'
-              placeholder='Task 1. Task 2. Task 3'
-              defaultValue={
-                this.props.data === undefined
-                  ? ""
-                  : this.props.data.tasks.join(". ")
-              }
-            ></input>
-          </div>
+        <div className='form__row form__row--tasks'>
+          <label>
+            Main Tasks:
+            <br />
+            <span className='small-text'>Separate with periods</span>
+          </label>
+          <input
+            className='text-input'
+            type='text'
+            name='tasksInput'
+            id='tasksInput'
+            placeholder='Task 1. Task 2. Task 3'
+            defaultValue={
+              props.data === undefined ? "" : props.data.tasks.join(". ")
+            }
+          ></input>
+        </div>
 
-          <div className='form__row'>
-            <label>From: </label>
-            <input
-              className='text-input'
-              type='text'
-              name='expDateFrom'
-              id='expDateFrom'
-              placeholder='June 2011'
-              defaultValue={
-                this.props.data === undefined ? "" : this.props.data.dates[0]
-              }
-            ></input>
-          </div>
+        <div className='form__row'>
+          <label>From: </label>
+          <input
+            className='text-input'
+            type='text'
+            name='expDateFrom'
+            id='expDateFrom'
+            placeholder='June 2011'
+            defaultValue={props.data === undefined ? "" : props.data.dates[0]}
+          ></input>
+        </div>
 
-          <div className='form__row'>
-            <label>To: </label>
-            <input
-              className='text-input'
-              type='text'
-              name='expDateTo'
-              id='expDateTo'
-              placeholder='Present'
-              defaultValue={
-                this.props.data === undefined ? "" : this.props.data.dates[1]
-              }
-            ></input>
-          </div>
+        <div className='form__row'>
+          <label>To: </label>
+          <input
+            className='text-input'
+            type='text'
+            name='expDateTo'
+            id='expDateTo'
+            placeholder='Present'
+            defaultValue={props.data === undefined ? "" : props.data.dates[1]}
+          ></input>
+        </div>
 
-          <div className='form__btn__container'>
-            <button onClick={this.onCancel} type='button' id='cancel-general'>
-              Cancel
-            </button>
-            <input type='submit'></input>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        <div className='form__btn__container'>
+          <button onClick={onCancel} type='button' id='cancel-general'>
+            Cancel
+          </button>
+          <input type='submit'></input>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export { ExperienceForm };
--- src/components/GeneralForm.js
+++ src/components/GeneralForm.js
@@ -1,134 +1,99 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class GeneralForm extends Component {
-  constructor(props) {
-    super(props);
+const GeneralForm = function (props) {
+  let [name, setName] = useState(props.data.name);
+  let [email, setEmail] = useState(props.data.email);
+  let [tel, setTel] = useState(props.data.tel);
+  let [street, setStreet] = useState(props.data.address[0]);
+  let [city, setCity] = useState(props.data.address[1]);
 
-    this.state = {
-      name: props.data.name,
-      email: props.data.email,
-      tel: props.data.tel,
-      street: props.data.address[0],
-      city: props.data.address[1]
-    };
-
-    this.handleChangeName = this.handleChangeName.bind(this);
-    this.handleChangeEmail = this.handleChangeEmail.bind(this);
-    this.handleChangeTel = this.handleChangeTel.bind(this);
-    this.handleChangeStreet = this.handleChangeStreet.bind(this);
-    this.handleChangeCity = this.handleChangeCity.bind(this);
-  }
-
-  onCancel = (e) => {
-    this.props.parentCallbackGenCancel(false);
-  };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     // Transfer input data to callback for parent
-    this.props.parentCallbackGen(this.state);
+    const data = { name, email, tel, street, city };
+    props.parentCallbackGen(data);
   };
 
-  handleChangeName(e) {
-    this.setState({ name: e.target.value });
-  }
+  const onCancel = (e) => props.parentCallbackGenCancel(false);
 
-  handleChangeEmail(e) {
-    this.setState({ email: e.target.value });
-  }
+  return (
+    <div className='form'>
+      <form onSubmit={onSubmit}>
+        <div className='form__row'>
+          <label>Name: </label>
+          <input
+            className='text-input'
+            type='text'
+            name='nameInput'
+            autoComplete='name'
+            placeholder='Doctor Strange'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          ></input>
+        </div>
 
-  handleChangeTel(e) {
-    this.setState({ tel: e.target.value });
-  }
+        <div className='form__row'>
+          <label>Email: </label>
+          <input
+            className='text-input'
+            type='email'
+            name='emailOnput'
+            autoComplete='email'
+            placeholder='strangedoctor@mcu.com'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          ></input>
+        </div>
 
-  handleChangeStreet(e) {
-    this.setState({ street: e.target.value });
-  }
+        <div className='form__row'>
+          <label>Phone: </label>
+          <input
+            className='text-input'
+            type='text'
+            name='telInput'
+            autoComplete='tel'
+            placeholder='(413) 666-3091'
+            onChange={(e) => setTel(e.target.value)}
+            value={tel}
+          ></input>
+        </div>
 
-  handleChangeCity(e) {
-    this.setState({ city: e.target.value });
-  }
+        <div className='form__row'>
+          <label>Street Address: </label>
+          <input
+            className='text-input'
+            type='text'
+            name='streetInput'
+            autoComplete='street-address'
+            placeholder='127 Bleaker St Apt 1'
+            onChange={(e) => setStreet(e.target.value)}
+            value={street}
+          ></input>
+        </div>
 
-  render() {
-    return (
-      <div className='form'>
-        <form onSubmit={this.onSubmit}>
-          <div className='form__row'>
-            <label>Name: </label>
-            <input
-              className='text-input'
-              type='text'
-              name='nameInput'
-              autoComplete='name'
-              placeholder='Doctor Strange'
-              onChange={this.handleChangeName}
-              value={this.state.name}
-            ></input>
-          </div>
+        <div className='form__row'>
+          <label>City, State, ZIP: </label>
+          <input
+            className='text-input'
+            type='text'
+            name='cityInput'
+            id='cityInput'
+            placeholder='New York, NY 10481'
+            onChange={(e) => setCity(e.target.value)}
+            value={city}
+          ></input>
+        </div>
 
-          <div className='form__row'>
-            <label>Email: </label>
-            <input
-              className='text-input'
-              type='email'
-              name='emailOnput'
-              autoComplete='email'
-              placeholder='strangedoctor@mcu.com'
-              onChange={this.handleChangeEmail}
-              value={this.state.email}
-            ></input>
-          </div>
-
-          <div className='form__row'>
-            <label>Phone: </label>
-            <input
-              className='text-input'
-              type='text'
-              name='telInput'
-              autoComplete='tel'
-              placeholder='(413) 666-3091'
-              onChange={this.handleChangeTel}
-              value={this.state.tel}
-            ></input>
-          </div>
-
-          <div className='form__row'>
-            <label>Street Address: </label>
-            <input
-              className='text-input'
-              type='text'
-              name='streetInput'
-              autoComplete='street-address'
-              placeholder='127 Bleaker St Apt 1'
-              onChange={this.handleChangeStreet}
-              value={this.state.street}
-            ></input>
-          </div>
-
-          <div className='form__row'>
-            <label>City, State, ZIP: </label>
-            <input
-              className='text-input'
-              type='text'
-              name='cityInput'
-              id='cityInput'
-              placeholder='New York, NY 10481'
-              onChange={this.handleChangeCity}
-              value={this.state.city}
-            ></input>
-          </div>
-
-          <div className='form__btn__container'>
-            <button onClick={this.onCancel} type='button' id='cancel-general'>
-              Cancel
-            </button>
-            <input type='submit' id='submit-general'></input>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        <div className='form__btn__container'>
+          <button onClick={onCancel} type='button' id='cancel-general'>
+            Cancel
+          </button>
+          <input type='submit' id='submit-general'></input>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export { GeneralForm };
